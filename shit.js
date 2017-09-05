ab0eee1891aac84f6b8ac9039783a8353047b989
AJ-SR <cubscoutaj@gmail.com>
2017-09-05 11:03:05 -0500

    upload of main files

---
+++ shit.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const prefix = "-";
+const fs = require("fs");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+client.login(config.token);
