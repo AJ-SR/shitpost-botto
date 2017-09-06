7adaf8f6741a2d97e21342c558a53e0a4d3d34f1
AJ-SR <cubscoutaj@gmail.com>
2017-09-05 22:04:42 -0500

    working on auto responses

--- shit.js
+++ shit.js
@@ -4,6 +4,11 @@ const config = require("./config.json");
 const auth = require("./auth.json");
 const prefix = "-";
 const fs = require("fs");
+const reponse = {
+	"shitpost" : "Shitpost. You mean AJ's poetry?",
+	"Desoros" : "Desoros. I like that guy."
+};
+
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -13,17 +18,25 @@ client.on("ready", () => {
 function isShitpost(message) {
   for (i = 0; i < config.keywords.length; i++) {
     if (message.content.includes(config.keywords[i])) {
-      return true;
+     return true;
+		}
+	}
 }
 
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  if (message.author.bot) return;
 
   if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send("pong!");
   } else
   if (message.content.startsWith(config.prefix + "foo")) {
     message.channel.send("bar!");
+  } else
+  /* if (isShitpost(message) == true) {
+  	message.channel.send("true");
+  } else */
+  if (reponse[message.content]) {
+  	message.channel.send(reponse[message.content]);
   }
 });
 
