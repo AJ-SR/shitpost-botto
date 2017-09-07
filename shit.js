66bec30c4465a3aaeabdd5178527fde9c7ef6690
AJ-SR <cubscoutaj@gmail.com>
2017-09-07 13:13:05 -0500

    removed some extra func

--- shit.js
+++ shit.js
@@ -6,46 +6,32 @@ const prefix = "-";
 const fs = require("fs");
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
-const response = {
-	"shitpost" : "Shitpost. You mean AJ's poetry?",
-	"Desoros" : "Desoros. I like that guy."
-};
+const response = require("./response.json");
 
 
 client.on("ready", () => {
   console.log("I am ready!");
-  client.user.setGame('shitposts');
+  client.user.setGame("shitposts");
 });
 
 function isShitpost(message) {
   for (i = 0; i < config.keywords.length; i++) {
     if (message.content.includes(config.keywords[i])) {
-     return true;
+     console.log("true");
 		}
 	}
 }
+
 client.on("message", (message) => {
 if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 	
 if (command === "ping")) {
-    	message.channel.send("pong!");
+	message.channel.send("pong!");
   } else
 if (command === "foo")) {
 	message.channel.send("bar!");
   }
 }
 
-
-client.on("message", (message) => {
-  if (message.author.bot) return;
-	
-  if (isShitpost(message) === true) {
-  	message.channel.send("true");
-  } else
-	  
-  if (response[message.content]) {
-  	message.channel.send(response[message.content]);
-  }
-});
 
 client.login(auth.token);
